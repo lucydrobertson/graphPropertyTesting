dcca70b9cdc3ca9d69bfd857ce83d6e8eb53e6f0
Lucy Robertson <lucy.robertson135@gmail.com>
2024-10-29 10:57:36 +0000

    quick mockup of 2-col test

--- denseGraphTesters/denseGraphTester.py
+++ denseGraphTesters/denseGraphTester.py
@@ -13,18 +13,37 @@ class DenseGraphTester:
         # construct an empty adjacency matrix
         subgraph_adj_matrix = [[False] * num_vertices] * num_vertices
         # construct the subgraph adjacency matrix from the full graph
-        for v1 in vertices:
-            for v2 in vertices:
-                subgraph_adj_matrix[v1][v2] = self.graph.is_edge(v1, v2)
+        # use x and y so that subgraph has vertices labelled 0 ... n
+        for x in range(0, num_vertices):
+            for y in range(0, num_vertices):
+                subgraph_adj_matrix[x][y] = self.graph.is_edge(vertices[x], vertices[y])
         return DenseGraph(num_vertices, subgraph_adj_matrix)
 
     def test_bipartiteness(self):
         num_select = 1/(self.epsilon ** 2)
         vertices_chosen = [random.randint(0, self.graph.get_size()) for _ in range(num_select)]
-        # remove duplicates by making vertices_chosen a set
-        vertices_chosen = set(vertices_chosen)
+        # remove duplicates, unlikely to be many when tester is used on large graph as intended
+        vertices_chosen = list(set(vertices_chosen))
         subgraph = self.construct_induced_subgraph(vertices_chosen)
 
         # perform bfs to decide if subgraph is bipartite
         # 2-col == bipartite
+        # start from vertex 0, an arbitrary choice
+        queue = [0]
+        colouring = {0: 0}
+        while len(queue) > 0:
+            current = queue.pop(0)
+            for n in range(subgraph.get_size()):
+                if subgraph.is_edge(current, n):
+                    try:
+                        if colouring[current] == colouring[n]:
+                            return False
+                    except KeyError:
+                        if colouring[current] == 0:
+                            colouring[n] = 1
+                        else:
+                            colouring[n] = 0
 
+                if n not in queue:
+                    queue.append(n)
+        return True
