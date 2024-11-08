13335f9ab343409283cb2f7479f7dc9910d1770d
Lucy Robertson <lucy.robertson135@gmail.com>
2024-11-08 13:48:20 +0000

    made bd graphs optionally directed and added in extra comments

--- boundedDegreeGraphs/boundedDegreeGraph.py
+++ boundedDegreeGraphs/boundedDegreeGraph.py
@@ -13,17 +13,20 @@ class BoundedDegreeGraph:
 
     def add_neighbour(self, vertex, new_neighbour):
         try:
+            # add in the new neighbour to an undirected graph
+            # iff both the vertex and the new neighbour have space
             if (len(self.inc_func[vertex]) < self.degree and len(self.inc_func[new_neighbour]) <
                     self.degree and not self.directed):
                 self.inc_func[new_neighbour].append(vertex)
                 self.inc_func[vertex].append(new_neighbour)
+            # otherwise if graph is undirected, add in new neighbour if vertex has space
             elif self.directed and len(self.inc_func[vertex]) < self.degree:
                 self.inc_func[vertex].append(new_neighbour)
             else:
                 raise ValueError("The degree of vertex {vertex} is already bounded by the graph's maximum degree")
 
         except KeyError:
-            raise KeyError(f"The vertex {vertex} is not present in the incidence function")
+            raise KeyError(f"The vertex {vertex}  or its neighbour {new_neighbour} is not present in the incidence function")
 
     def get_size(self):
         return self.size()
