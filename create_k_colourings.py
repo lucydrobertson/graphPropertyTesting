1401b601b5a43ee82c8c26d977a1b24c187f67fb
Lucy Robertson <lucy.robertson135@gmail.com>
2025-03-27 11:50:25 +0000

    generate single colourings from index

--- create_k_colourings.py
+++ create_k_colourings.py
@@ -2,17 +2,18 @@ def generate_k_colourings(graph_size, k):
     # generate each potential k-colouring of a graph of size graph_size
     possible_colourings = []
 
-    subgraph_size = graph_size
-    for index in range(k ** subgraph_size):
-        colouring = [0] * subgraph_size
-        # convert the index (number of colouring) into an array representing that colouring
-        # aka convert the index from base 10 to base k
-        col_index = -1
-        while index > 0:
-            colouring[col_index] = index % k
-            index = index // k
-            col_index -= 1
-
+    for index in range(k ** graph_size):
+        colouring = generate_k_colouring_from_index(index, graph_size, k)
         possible_colourings.append(colouring)
-
     return possible_colourings
+
+
+def generate_k_colouring_from_index(index, graph_size, k):
+    colouring = [0] * graph_size
+    # convert the index (number of colouring) into an array representing that colouring
+    col_index = -1
+    while index > 0:
+        colouring[col_index] = index % k
+        index = index // k
+        col_index -= 1
+    return colouring
